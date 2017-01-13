50bc230426f16be3487600b4fca0bff6c12b7ee0
Travis Harrods <travis.harrods@gmail.com>
2017-01-13 12:08:04 +0100

    Create the FeatureSubsets class, extented from Subsets abstract class.

---
+++ lib/Travis/Bio/SequenceFactory/FeatureSubsets.pm
@@ -0,0 +1,20 @@
+package Travis::Bio::SequenceFactory::Subsets;
+
+#==============================================================================
+# Travis::Bio::SequenceFactory::FeatureSubsets extends from Subsets class and
+# handles Bio::SeqFeature objects.
+#
+# Author: Hugo Devillers, Travis Harrods
+# Created: 13-JAN-2017
+# Updated:
+#==============================================================================
+
+#==============================================================================
+# REQUIERMENTS
+#==============================================================================
+use Moose;
+
+extends Travis::Bio::SequenceFactory::Subsets;
+
+no Moose;
+return(1);
